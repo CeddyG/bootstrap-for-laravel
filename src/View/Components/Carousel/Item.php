62ba77e9364e9142ecec1625ad3234d87c609044
CeddyG <ceddyg@free.fr>
2023-07-15 00:42:14 +0200

    -Add Carousel item class

---
+++ src/View/Components/Carousel/Item.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CeddyG\Bootstrap\View\Components\Carousel;
+
+use Illuminate\View\Component;
+
+class Item extends Component
+{
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('bs::components.carousel.item');
+    }
+}
