09723eaa90b522de600ad4a6d76b5f2a02c3a6a3
CeddyG <ceddyg@free.fr>
2023-07-15 00:39:06 +0200

    -Add Progress class

---
+++ src/View/Components/Progress.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CeddyG\Bootstrap\View\Components;
+
+use Illuminate\View\Component;
+
+class Progress extends Component
+{
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('bs::components.progress');
+    }
+}
